6bdf19498de10d58c07c0d1147627b22d475c3da
张锦洲 <17225278+Zjz0211@user.noreply.gitee.com>
2026-07-21 12:49:28 +0800

    fix: remove missing AlimamaFangYuanTi font, use Noto Sans SC

--- app/components/blog/BlogHeader.global.vue
+++ app/components/blog/BlogHeader.global.vue
@@ -67,13 +67,9 @@ const appConfig = useAppConfig()
 	}
 }
 
-@font-face {
-	font-family: AlimamaFangYuanTi;
-	src: url("/fonts/AlimamaFangYuanTi.woff2");
-}
 
 .header-title {
-	font-family: AlimamaFangYuanTi, "Noto Sans SC", sans-serif;
+	font-family: "Noto Sans SC", sans-serif;
 	font-size: 1.5em;
 	font-synthesis: none;
 	font-variation-settings: "wght" 600, "BEVL" 100;
